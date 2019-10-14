c768b5736c7b0247a49ef148627d8dd4aec10804
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-14 17:38:00 -0400

    set up routes for params. completed mvp goals

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -20,8 +21,8 @@ const MovieList = props => {
   
   return (
     <div className="movie-list">
-      {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map((movie) => (
+        <Link to={`/movies/${movie.id}`} key={movie.id}><MovieDetails movie={movie} /></Link>
       ))}
     </div>
   );
@@ -40,7 +41,7 @@ function MovieDetails({ movie }) {
       </div>
       <h3>Actors</h3>
 
-      {stars.map(star => (
+      {stars.map((star) => (
         <div key={star} className="movie-star">
           {star}
         </div>
