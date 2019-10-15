3dacd47f306be6929019290c7dcdf20be0ae11ad
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-15 18:48:05 -0400

    completed functionality for the save button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -24,10 +24,10 @@ const Movie = (props) => {
   }, [props.match.params.movieId]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -38,7 +38,7 @@ const Movie = (props) => {
   return (
     <div className="save-wrapper">
       <MovieCard title={title} director={director} metascore={metascore} stars={stars} />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={() => saveMovie()}>Save</div>
     </div>
   );
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+    {props.list.map((movie, index) => (
+      <NavLink to={`/movies/${movie.id}`} key={index} className="saved-movie">{movie.title}</NavLink>
     ))}
     <Link to='/' className="home-button">Home</Link>
   </div>
